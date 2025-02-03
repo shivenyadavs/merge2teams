0748aa6b8b259f8b2950e26913adc893e2c7f73e
SHIVEN YADAV S <155547804+shivenyadavs@users.noreply.github.com>
2025-02-03 15:58:39 +0530

    Delete add.py

--- add.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(a, b):
-    return a + b
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-sum_result = add_numbers(num1, num2)
-print(f"The sum of {num1} and {num2} is {sum_result}")
